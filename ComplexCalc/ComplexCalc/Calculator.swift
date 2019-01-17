bbb614a1f41ee5f3d30c94cb2ee75abc034890d3
juan3674 <juan3674@uw.edu>
2019-01-17 08:52:19 -0800

    finished basic calc

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,48 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    func add (lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract (lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply (lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide (lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func add (_ args: [Int]) -> Int {
+        var sum = 0
+        for index in 0..<args.count - 1 {
+            sum += args[index]
+        }
+        return sum
+    }
+    
+    func multiply (_ args: [Int]) -> Int {
+        var mult = 0
+        for index in 0..<args.count - 1 {
+            mult *= args[index]
+        }
+        return mult
+    }
+    
+    func count (_ args: [Int]) -> Int {
+        return args.count
+    }
+    
+    func avg (_ args: [Int]) -> Int {
+        var sum = 0
+        for index in 0..<args.count - 1 {
+            sum += args[index]
+        }
+        return sum / (args.count)
+    }
+
 }
