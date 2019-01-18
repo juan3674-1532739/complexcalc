fb16efe0da253a084ed0e65e0b07b68892b1c960
juan3674 <juan3674@uw.edu>
2019-01-17 16:04:23 -0800

    finished

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -28,15 +28,15 @@ class Calculator {
     
     func add (_ args: [Int]) -> Int {
         var sum = 0
-        for index in 0..<args.count - 1 {
+        for index in 0..<args.count {
             sum += args[index]
         }
         return sum
     }
     
     func multiply (_ args: [Int]) -> Int {
-        var mult = 0
-        for index in 0..<args.count - 1 {
+        var mult = 1
+        for index in 0..<args.count {
             mult *= args[index]
         }
         return mult
@@ -44,6 +44,14 @@ class Calculator {
     
     func count (_ args: [Int]) -> Int {
         return args.count
+    }
+    
+    func avg (_ args: [Int]) -> Int {
+        var sum = 0
+        for index in 0..<args.count {
+            sum += args[index]
+        }
+        return sum / (args.count)
     }
     
     func add (lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
@@ -54,12 +62,23 @@ class Calculator {
         return (lhs.0 - rhs.0, lhs.1 - rhs.1)
     }
     
-    func avg (_ args: [Int]) -> Int {
-        var sum = 0
-        for index in 0..<args.count - 1 {
-            sum += args[index]
-        }
-        return sum / (args.count)
+    func add (lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": (lhs["x"]! + rhs["x"]!), "y": (lhs["y"]! + rhs["y"]!) ]
     }
-
+    
+    func subtract (lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": (lhs["x"]! - rhs["x"]!), "y": (lhs["y"]! - rhs["y"]!) ]
+    }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var sum: Int = beg
+        for index in 0..<args.count {
+            sum = op(sum, args[index])
+        }
+        return sum
+    }
 }
